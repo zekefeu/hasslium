1dc260588af2711c9f124de4860b417d87b32fd7
fuka <fuka_main@protonmail.com>
2022-05-20 21:04:40 +0200

    Handle warning, error, define and undef

--- src/preprocessor/preprocessor.js
+++ src/preprocessor/preprocessor.js
@@ -16,13 +16,13 @@ export function processOne(input, options, callback) {
     // if (input.length === 0) callback("The input is empty.", null);
     // if (!options) callback("No options provided", null);
     if (verbose) {
-        console.log("v-001 Verbose mode:", verbose);
-        console.log("\nv-002 Raw options:", options);
-        console.log("\nv-003 Defined macros:");
+        console.log("v-001 │ Verbose mode:", verbose);
+        console.log("\nv-002 │ Raw options:", options);
+        console.log("\nv-003 │ Defined macros:");
         options.macros.forEach(macro => {
-            console.log(`v-013 - ${macro[0]}: ${macro[1]}`);
+            console.log(`v-013 │ - ${macro[0]}: ${macro[1]}`);
         });
-        console.log("\nv-004 Input:", input);
+        console.log("\nv-004 │ Input:", input);
     }
     // Initialization
     // eslint-disable-next-line prefer-const
@@ -31,16 +31,20 @@ export function processOne(input, options, callback) {
     let outputFile = [];
     if (options.macros.length > 0) {
         options.macros.forEach(macro => {
-            //console.log(macro);
-            activeMacros.push(macro);
+            if (macro[0] && macro[1]) {
+                activeMacros.push(macro);
+            }
+            else {
+                console.log("Error parsing provided macro:", macro);
+            }
         });
     }
     if (verbose)
-        console.log("\nv-005 === Preprocessing ===\n");
+        console.log("\nv-005 │ === Preprocessing ===\n");
     // Loop through each given line
     input.forEach(line => {
         if (verbose)
-            console.log("v-006 ==>", line);
+            console.log("v-006 ├─➤", line);
         // Line without indentation
         const trimmedLine = line.trim();
         if (trimmedLine.startsWith("//#")) {
@@ -70,40 +74,118 @@ export function processOne(input, options, callback) {
                 directiveArray.shift(),
                 directiveArray.join(" ") // arguments: put the array back together
             ];
-            if (verbose) {
-                console.log("v-007 Parsed directive:", currentDirective);
-            }
+            if (verbose)
+                console.log("v-007 │ Parsed directive:", currentDirective);
             switch (currentDirective[0]) {
-                case "define":
+                case "define": {
                     if (verbose)
-                        console.log("v-008 Directive: define");
+                        console.log("v-008 │ Directive: define");
+                    const argumentsArray = currentDirective[1].split(" ");
+                    // Defining the macro we want to add
+                    const macro = [
+                        argumentsArray.shift(),
+                        argumentsArray.join(" ") // value
+                    ];
+                    if (macro[0] && macro[1]) {
+                        activeMacros.push(macro);
+                        if (verbose)
+                            console.log("v-016 │ New macros list:", activeMacros);
+                    }
+                    else {
+                        if (verbose)
+                            console.log("v-015 │ Unknown macro definition:", macro);
+                    }
                     break;
-                case "undef":
+                }
+                case "undef": {
                     if (verbose)
-                        console.log("v-009 Directive: undef");
+                        console.log("v-009 │ Directive: undef");
+                    // Argument array (first value contains the macro we want to remove, we don't care about the rest)
+                    const argumentsArray = currentDirective[1].split(" ");
+                    if (argumentsArray[0]) {
+                        // Filtering the macros by their key
+                        activeMacros = activeMacros.filter((macro) => { return macro[0] !== argumentsArray[0]; });
+                        if (verbose) {
+                            console.log("v-018 │ Removing macro:", argumentsArray[0]);
+                            console.log("v-019 │ New macros list:", activeMacros);
+                        }
+                    }
+                    else {
+                        if (verbose)
+                            console.log("v-020 │ Unknown macro:", argumentsArray);
+                    }
                     break;
-                case "if":
+                }
+                case "if": {
                     if (verbose)
-                        console.log("v-010 Directive: if");
+                        console.log("v-010 │ Directive: if");
                     break;
-                case "ifdef":
+                }
+                case "elif": {
                     if (verbose)
-                        console.log("v-011 Directive: ifdef");
+                        console.log("v-023 │ Directive: elif");
                     break;
-                default:
+                }
+                case "endif": {
                     if (verbose)
-                        console.log("v-012 Unknown directive.");
+                        console.log("v-024 │ Directive: endif");
                     break;
+                }
+                case "else": {
+                    if (verbose)
+                        console.log("v-025 │ Directive: else");
+                    break;
+                }
+                case "ifdef": {
+                    if (verbose)
+                        console.log("v-011 │ Directive: ifdef");
+                    break;
+                }
+                case "ifndef": {
+                    if (verbose)
+                        console.log("v-026 │ Directive: ifndef");
+                    break;
+                }
+                case "warning": {
+                    if (verbose)
+                        console.log("v-021 │ Directive: warning");
+                    let message = currentDirective[1];
+                    // If no warning is specified, return a generic message
+                    if (!message)
+                        message = "Unknown error.";
+                    // Log to stderr and callback
+                    console.error("Warning: " + message);
+                    break;
+                }
+                case "error": {
+                    if (verbose)
+                        console.log("v-022 │ Directive: error");
+                    let message = currentDirective[1];
+                    // If no error is specified, return a generic message
+                    if (!message)
+                        message = "Unknown error.";
+                    // Log to stderr and callback
+                    console.error("Error: " + message);
+                    callback("Error: " + message, null);
+                    break;
+                }
+                default: {
+                    // Just ignore it and move on
+                    if (verbose)
+                        console.log("v-012 │ Unknown directive.");
+                    break;
+                }
             }
         }
         else {
             // Line is not a macro
+            // Apply macro filters, etc
         }
     });
     if (verbose) {
         const endTime = new Date();
         const timeTook = endTime.getTime() - startTime.getTime();
-        console.log("Preprocessing took " + timeTook + " ms.");
+        console.log("\nv-017 │ === Preprocessing took " + timeTook + " ms ===\n");
     }
     callback(null, outputFile);
 }
--- src/preprocessor/preprocessor.ts
+++ src/preprocessor/preprocessor.ts
@@ -5,8 +5,13 @@ export type directiveType = [
 	arguments: string
 ]
 
+export type macroType = [
+	macro: string,
+	token: string
+]
+
 export interface processOptions {
-	macros?: directiveType[],
+	macros?: macroType[],
 	verbose?: boolean
 }
 
@@ -31,16 +36,16 @@ export function processOne(input: string[], options: processOptions, callback: (
 	// if (!options) callback("No options provided", null);
 	
 	if (verbose) {
-		console.log("v-001 Verbose mode:", verbose);
-		console.log("\nv-002 Raw options:", options);
+		console.log("v-001 │ Verbose mode:", verbose);
+		console.log("\nv-002 │ Raw options:", options);
 	
-		console.log("\nv-003 Defined macros:");
+		console.log("\nv-003 │ Defined macros:");
 		
 		options.macros.forEach(macro => {
-			console.log(`v-013 - ${macro[0]}: ${macro[1]}`);
+			console.log(`v-013 │ - ${macro[0]}: ${macro[1]}`);
 		});
 
-		console.log("\nv-004 Input:", input);
+		console.log("\nv-004 │ Input:", input);
 	}
 
 	// Initialization
@@ -53,16 +58,19 @@ export function processOne(input: string[], options: processOptions, callback: (
 
 	if (options.macros.length > 0) {
 		options.macros.forEach(macro => {
-			//console.log(macro);
-			activeMacros.push(macro);
+			if (macro[0] && macro[1]) {
+				activeMacros.push(macro);
+			} else {
+				console.log("Error parsing provided macro:", macro);
+			}
 		});
 	}
 
-	if (verbose) console.log("\nv-005 === Preprocessing ===\n");
+	if (verbose) console.log("\nv-005 │ === Preprocessing ===\n");
 
 	// Loop through each given line
 	input.forEach(line => {
-		if (verbose) console.log("v-006 ==>", line);
+		if (verbose) console.log("v-006 ├─➤", line);
 
 		// Line without indentation
 		const trimmedLine = line.trim();
@@ -100,36 +108,114 @@ export function processOne(input: string[], options: processOptions, callback: (
 				directiveArray.join(" ") // arguments: put the array back together
 			];
 
-			if (verbose) {
-				console.log("v-007 Parsed directive:", currentDirective);
-			}
+			if (verbose) console.log("v-007 │ Parsed directive:", currentDirective);
 			
 			switch (currentDirective[0]) {
-				case "define":
-					if (verbose) console.log("v-008 Directive: define");
+				case "define": {
+					if (verbose) console.log("v-008 │ Directive: define");
+					
+					const argumentsArray = currentDirective[1].split(" ");
+
+					// Defining the macro we want to add
+					const macro: macroType = [
+						argumentsArray.shift(),  // key
+						argumentsArray.join(" ") // value
+					];
+
+					if (macro[0] && macro[1]) {
+						activeMacros.push(macro);
+						if (verbose)  console.log("v-016 │ New macros list:", activeMacros);
 						
+					} else {
+						if (verbose) console.log("v-015 │ Unknown macro definition:", macro);
+					}
 					break;
-				
-				case "undef":
-					if (verbose) console.log("v-009 Directive: undef");
+				}
+				case "undef": {
+					if (verbose) console.log("v-009 │ Directive: undef");
+					
+					// Argument array (first value contains the macro we want to remove, we don't care about the rest)
+					const argumentsArray = currentDirective[1].split(" ");
+
+					if (argumentsArray[0]) {
+						
+						// Filtering the macros by their key
+						activeMacros = activeMacros.filter((macro) => { return macro[0] !== argumentsArray[0]; });
+						
+						if (verbose) {
+							console.log("v-018 │ Removing macro:", argumentsArray[0]);
+							console.log("v-019 │ New macros list:", activeMacros);
+						}
+					} else {
+						if (verbose) console.log("v-020 │ Unknown macro:", argumentsArray);
+					}
+					break;
+				}
+				case "if": {
+					if (verbose) console.log("v-010 │ Directive: if");
+					
+					break;
+				}
+				case "elif": {
+					if (verbose) console.log("v-023 │ Directive: elif");
 
 					break;
-				
-				case "if":
-					if (verbose) console.log("v-010 Directive: if");
+				}
+				case "endif": {
+					if (verbose) console.log("v-024 │ Directive: endif");
+					
+					break;
+				}
+				case "else": {
+					if (verbose) console.log("v-025 │ Directive: else");
+					
+					break;
+				}
+				case "ifdef": {
+					if (verbose) console.log("v-011 │ Directive: ifdef");
 
 					break;
-				
-				case "ifdef":
-					if (verbose) console.log("v-011 Directive: ifdef");
+				}
+				case "ifndef": {
+					if (verbose) console.log("v-026 │ Directive: ifndef");
+
 					break;
-				
-				default:
-					if (verbose) console.log("v-012 Unknown directive.");
+				}
+				case "warning": {
+					if (verbose) console.log("v-021 │ Directive: warning");
+
+					let message = currentDirective[1];
+
+					// If no warning is specified, return a generic message
+					if (!message) message = "Unknown error.";
+
+					// Log to stderr and callback
+					console.error("Warning: " + message);
 					break;
+				}
+				case "error": {
+					if (verbose) console.log("v-022 │ Directive: error");
+
+					let message = currentDirective[1];
+						
+					// If no error is specified, return a generic message
+					if (!message) message = "Unknown error.";
+						
+					// Log to stderr and callback
+					console.error("Error: " + message);
+					callback("Error: " + message, null);
+					break;
+				}
+				default: {
+					// Just ignore it and move on
+					if (verbose) console.log("v-012 │ Unknown directive.");
+					break;
+				}
 			}
 		} else {
 			// Line is not a macro
+
+			// Apply macro filters, etc
 		}
 	});
 	
@@ -137,8 +223,10 @@ export function processOne(input: string[], options: processOptions, callback: (
 		const endTime = new Date();
 		const timeTook = endTime.getTime() - startTime.getTime();
 		
-		console.log("Preprocessing took " + timeTook + " ms.");
+		console.log("\nv-017 │ === Preprocessing took " + timeTook + " ms ===\n");
 	}
 	
 	callback(null, outputFile);
 }
+
+//26
