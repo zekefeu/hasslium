d14b1d27ea5b78c2656a149d69e4290e5b9c2261
fuka <fuka_main@protonmail.com>
2022-05-22 22:22:54 +0200

    BREAKING: Remove processOne()

--- src/preprocessor/preprocessor.ts
+++ src/preprocessor/preprocessor.ts
@@ -18,14 +18,6 @@ export interface processOptions {
 }
 
 /**
- * @deprecated use hasslium.process() instead
- */
-export function processOne(input: string[], options: processOptions, callback: (error: string, output: string[]) => void) {
-	console.error(new Error("DeprecationWarning: hasslium.processOne() is deprecated. Use hasslium.process() instead."));
-	process(input, options, callback);
-}
-
-/**
  * The library's main function. Takes an array of strings and processes it with the given options.
  * @param input The input array
  * @param options See above
