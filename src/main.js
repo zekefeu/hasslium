a47c4d5a1331d7d74faec823df79dc747968b9cc
fuka <fuka_main@protonmail.com>
2022-05-22 18:47:18 +0200

    Remove test function

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
 "use strict";
+
 // Exports
 export * from "./preprocessor/preprocessor.js";
-
-/*
-// Test main process function (dev only)
-import * as preprocess from "./preprocessor/preprocessor.js";
-import * as fs from "fs";
-
-preprocess.process(fs.readFileSync("./test/assets/1.js").toString().split("\n"), { macros: [["hella", "hello"], ["Hoi", "Hi"]], verbose: true }, (error, output) => {
-    if (error) {
-        console.error(error);
-    }
-    else {
-        console.log("Output:", output);
-    }
-});
-*/
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,3 @@
 export * from "./preprocessor/preprocessor.js";
 
 
-// Test main process function (dev only)
-import * as preprocess from "./preprocessor/preprocessor.js";
-import * as fs from "fs";
-
-// eslint-disable-next-line no-constant-condition
-if (true) {
-	preprocess.process(fs.readFileSync("./test/assets/1.js").toString().split("\n"), { macros: [["hella", "hello"], ["Hoi", "Hi"]], verbose: true }, (error, output) => {
-		if (error) {
-			console.error(error);
-		} else {
-			console.log("Output:", output);
-		}
-	});
-}
